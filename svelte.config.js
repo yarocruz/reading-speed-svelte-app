a76071d5f398308917ff21baf0dbb95788a5effc
yarocruz <yarocruz@gmail.com>
2022-06-29 16:22:29 -0400

    pushing build

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-//const dev = process.env.NODE_ENV === 'development';
+// const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
