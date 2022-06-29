86517252dc3749ce05f8813019b85213f88b3a69
yarocruz <yarocruz@gmail.com>
2022-06-29 15:47:55 -0400

    fixing build folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+
+//const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
+	preprocess: preprocess(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+	kit: {
+		// paths: {
+		// 	base: dev ? '' : '/reading-speed-svelte-app',
+		// },
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			default: true,
+		},
+
 	}
 };
 
